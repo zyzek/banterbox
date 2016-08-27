d92de0295576c2e380acaae07a412c4a28b095c6
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-08-27 17:05:23 +1000

    Converted prototype laravel migrations to django.

--- banterbox/models.py
+++ banterbox/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.TextField(unique=True)
+    email = models.TextField(unique=True)
+    active = models.BooleanField()
+    password = models.TextField()
+    join_date = models.DateTimeField(auto_now_add=True)
+
+class PasswordReset(models.Model):
+    email = models.TextField()
+    token = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Status(models.Model):
+    name = models.TextField(unique=True)
+
+class Room(models.Model):
+    public_id = models.TextField(unique=True)
+    name = models.TextField()
+    owner_id = models.OneToOneField(User.id, models.SET_NULL, null=True)
+    status_id = models.OneToOneField(Status.name, models.SET_NULL, null=True)
+    private = models.BooleanField(default=False)
+    password_protected = models.BooleanField(default=False)
+    password = models.TextField(null=True)
+
+class Comment(models.Model):
+    room_id = models.OneToOneField(Room.id, models.CASCADE)
+    user_id = models.OneToOneField(User.id, models.SET_NULL, null=True)
+    public_id = models.TextField(unique=True)
+
+    content = models.TextField()
+    private = models.BooleanField(default=False)
+
+class Roles(models.Model):
+    name = models.TextField(unique=True)
+
+class UserRoomRole:
+    user_id = models.OneToOneField(User.id, models.CASCADE)
+    role_id = models.OneToOneField(Roles.id, models.CASCADE)
+    room_id = models.OneToOneField(Room.id, models.CASCADE)
