e1d2e5fe8e85f49873e63deefe9bc2d72d20c78c
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-08-30 15:53:43 +1000

    Improved models, added admin panel.

--- banterbox/admin.py
+++ banterbox/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(PasswordReset)
+admin.site.register(Status)
+admin.site.register(Room)
+admin.site.register(Comment)
+admin.site.register(Role)
+admin.site.register(UserRoomRole)
+admin.site.register(Class)
+admin.site.register(UserClassEnrolment)
+admin.site.register(ScheduledRoom)
+
--- banterbox/models.py
+++ banterbox/models.py
@@ -2,61 +2,92 @@ from django.db import models
 from uuid import uuid4
 
 class User(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    public_id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.TextField(unique=True)
     email = models.TextField(unique=True)
     active = models.BooleanField()
     password = models.TextField()
     join_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 class PasswordReset(models.Model):
     email = models.TextField()
     token = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "{} @ {}".format(self.email, self.created_at)
 
 class Status(models.Model):
     name = models.TextField(unique=True)
+    
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    public_id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.TextField()
-    lecturer_id = models.OneToOneField(User)
-    class_id = models.OneToOneField('Class', models.SET_NULL, null=True)
-    status_id = models.OneToOneField(Status, models.SET_NULL, null=True)
+    lecturer = models.OneToOneField(User)
+    room_class = models.OneToOneField('Class', models.SET_NULL, null=True)
+    status = models.OneToOneField(Status, models.SET_NULL, null=True)
     private = models.BooleanField(default=False)
     password_protected = models.BooleanField(default=False)
     password = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
 
 class Comment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
-    room_id = models.OneToOneField(Room, models.CASCADE)
-    user_id = models.OneToOneField(User, models.SET_NULL, null=True)
+    public_id = models.UUIDField(primary_key=True, default=uuid4)
+    room = models.OneToOneField(Room, models.CASCADE)
+    user = models.OneToOneField(User, models.SET_NULL, null=True)
 
     content = models.TextField()
     private = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.content[:40]
 
-class Roles(models.Model):
+class Role(models.Model):
     name = models.TextField(unique=True)
 
+    def __str__(self):
+        return self.name
+
 class UserRoomRole(models.Model):
-    user_id = models.OneToOneField(User, models.CASCADE)
-    role_id = models.OneToOneField(Roles, models.CASCADE)
-    room_id = models.OneToOneField(Room, models.CASCADE)
+    user = models.OneToOneField(User, models.CASCADE)
+    role = models.OneToOneField(Role, models.CASCADE)
+    room = models.OneToOneField(Room, models.CASCADE)
+    
+    def __str__(self):
+        return "{} is {} in {}".format(self.user.name, self.role.name, self.room.name)
 
 class Class(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    public_id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.TextField()
-    lecturer_id = models.OneToOneField(User, models.SET_NULL, null=True)
+    lecturer = models.OneToOneField(User, models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
 
 class UserClassEnrolment(models.Model):
-    class_id = models.OneToOneField(Class, models.CASCADE)
-    user_id = models.OneToOneField(User, models.CASCADE)
+    enroled_class = models.OneToOneField(Class, models.CASCADE)
+    enroled_user = models.OneToOneField(User, models.CASCADE)
+
+    def __str__(self):
+        return "{} takes {}".format(self.enroled_class.name, self.enroled_user.name)
 
 class ScheduledRoom(models.Model):
     day = models.PositiveIntegerField()
-    class_id = models.OneToOneField(Class, models.CASCADE)
+    room_class = models.OneToOneField(Class, models.CASCADE)
     start_time = models.TimeField()
     end_time = models.TimeField()
+
+    def __str__(self):
+        return "{}: {}, [{} - {}]".format(self.room_class.name, self.day, self.start_time, self.end_time)
+
     
