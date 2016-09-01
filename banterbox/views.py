91ef3ff4cfc5bf926eddfc247cdbcd66804f41a9
Wafik Salim <wafiks@gmail.com>
2016-09-01 18:36:40 +1000

    Added base view files

--- banterbox/views.py
+++ banterbox/views.py
@@ -1,12 +1,20 @@
 from django.http import HttpResponse
+from django.template import loader
+
 import datetime
 
 
 def index(request):
-    return HttpResponse("You're at the root dir!")
+    template = loader.get_template('index.html')
+    context = {
+        'generic_var' : 123125
+    }
+    return HttpResponse(template.render(context,template))
 
 
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+
