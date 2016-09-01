ed244f9db470c3018e193eb721b0bb7b17dc36b3
royporter <rpor2708@uni.sydney.edu.au>
2016-09-01 23:17:53 +1000

    Added django REST framework. Began serializing models

---
+++ banterbox/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from banterbox.models import *
+
+
+
+class ClassSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Class
+        fields = ('public_id', 'name', 'lecturer', 'created_at')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('public_id', 'name', 'email', 'active', 'active', 'password', 'join_date')\
+
+
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Room
+        field = ('public_id', 'name', 'lecturer', 'room_class', 'status', 'private', 'password_protected', 'password', 'created_at')
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        field = ('public_id', 'room', 'user', 'content', 'is_private')
--- banterbox/views.py
+++ banterbox/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
+from rest_framework import viewsets
+from banterbox.serializers import *
+
+
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset = Class.objects.all().order_by('-created_at')
+    serializer_class = ClassSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-join_date')
+    serializer_class = UserSerializer
+
+
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all().order_by('-created_at')
+    serializer_class = RoomSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().
+
 
 def index(request):
     return HttpResponse("You're at the root dir!")
