250a9fb23e5718a315d0c216bcd4d76a63feeb71
Wafik Salim <wafiks@gmail.com>
2016-09-01 18:11:33 +1000

    Start of frontend branch

--- banterbox/urls.py
+++ banterbox/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^time', views.current_datetime, name='date_time')
 ]
--- banterbox/views.py
+++ banterbox/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+import datetime
+
 
 def index(request):
     return HttpResponse("You're at the root dir!")
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
