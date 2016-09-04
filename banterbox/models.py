78d29d70419c330974e06949ddfeb7dbb5ff9301
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-09-04 17:52:16 +1000

    Changed dumb one-to-one fields to foreign keys.

--- banterbox/models.py
+++ banterbox/models.py
@@ -20,9 +20,9 @@ class RoomStatus(models.Model):
 class Room(models.Model):
     public_id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=255)
-    lecturer = models.OneToOneField(User)
-    room_unit = models.OneToOneField('Unit', models.SET_NULL, null=True)
-    status = models.OneToOneField(RoomStatus, models.SET_NULL, null=True)
+    lecturer = models.ForeignKey(User)
+    unit = models.ForeignKey('Unit', models.SET_NULL, null=True)
+    status = models.ForeignKey(RoomStatus, models.SET_NULL, null=True)
     private = models.BooleanField(default=False)
     password_protected = models.BooleanField(default=False)
     password = models.CharField(max_length=255, null=True)
@@ -33,8 +33,8 @@ class Room(models.Model):
 
 class Comment(models.Model):
     public_id = models.UUIDField(primary_key=True, default=uuid4)
-    room = models.OneToOneField(Room, models.CASCADE)
-    user = models.OneToOneField(User, models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, models.CASCADE)
+    user = models.ForeignKey(User, models.SET_NULL, null=True)
 
     content = models.TextField()
     private = models.BooleanField(default=False)
@@ -49,36 +49,40 @@ class UserRole(models.Model):
         return self.name
 
 class UserRoomRole(models.Model):
-    user = models.OneToOneField(User, models.CASCADE)
-    role = models.OneToOneField(UserRole, models.CASCADE)
-    room = models.OneToOneField(Room, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
+    room = models.ForeignKey(Room, models.CASCADE)
+
+    role = models.ForeignKey(UserRole, models.CASCADE)
     
+    class Meta:
+        unique_together = ('user', 'room')
+
     def __str__(self):
         return "{} is {} in {}".format(self.user.name, self.role.name, self.room.name)
 
 class Unit(models.Model):
     public_id = models.UUIDField(primary_key=True, default=uuid4)
     name = models.CharField(max_length=255)
-    lecturer = models.OneToOneField(User, models.SET_NULL, null=True)
+    lecturer = models.ForeignKey(User, models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
 
 class UserUnitEnrolment(models.Model):
-    unit = models.OneToOneField(Unit, models.CASCADE)
-    user = models.OneToOneField(User, models.CASCADE)
+    unit = models.ForeignKey(Unit, models.CASCADE)
+    user = models.ForeignKey(User, models.CASCADE)
 
     def __str__(self):
         return "{} takes {}".format(self.unit.name, self.user.name)
 
 class ScheduledRoom(models.Model):
     day = models.PositiveIntegerField()
-    room_unit = models.OneToOneField(Unit, models.CASCADE)
+    unit = models.ForeignKey(Unit, models.CASCADE)
     start_time = models.TimeField()
     end_time = models.TimeField()
 
     def __str__(self):
-        return "{}: {}, [{} - {}]".format(self.room_unit.name, self.day, self.start_time, self.end_time)
+        return "{}: {}, [{} - {}]".format(self.unit.name, self.day, self.start_time, self.end_time)
 
     
