132867f039f25c553c11fbf09d1a02e1deaf4ffa
Wafik Salim <wafiks@gmail.com>
2016-09-04 21:13:09 +1000

    Added classes to unit component

--- banterbox/static/js/vue_app/app.js
+++ banterbox/static/js/vue_app/app.js
@@ -5,9 +5,10 @@ var units = [
         "created_at": 1472731865.293062,
         "room_name": "Freeman Cape",
         "name": "Automated executive Local Area Network",
-        "icon": null,
-        "status" : "open",
-        "next_session" : {"day" : "MONDAY", "time" : "14:00"}
+        "code" : "AUTO3300",
+        "icon": "anchor",
+        "status": "open",
+        "next_session": {"day": "MONDAY", "time": "14:00"},
     },
     {
         "public_id": "d47b4587-de0d-430c-ae2a-30df50f45d12",
@@ -15,9 +16,10 @@ var units = [
         "created_at": 1472731865.294826,
         "room_name": "Newton Shore",
         "name": "Diverse needs-based parallelism",
-        "icon": null,
-        "status" : "open",
-        "next_session" : {"day" : "MONDAY", "time" : "14:00"}
+        "icon": "firefox",
+        "code": "DNBS2110",
+        "status": "open",
+        "next_session": {"day": "MONDAY", "time": "14:00"}
     },
     {
         "public_id": "c87c7781-365a-477c-8985-93bbb2f8b6e7",
@@ -25,9 +27,10 @@ var units = [
         "created_at": 1472731865.296625,
         "room_name": "Ramsey View",
         "name": "Focused next generation frame",
-        "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "WEDNESDAY", "time" : "10:00"}
+        "code": "FNGF3105",
+        "icon": "heartbeat",
+        "status": "closed",
+        "next_session": {"day": "WEDNESDAY", "time": "10:00"}
     },
     {
         "public_id": "0e8d3a9d-438e-409f-9bf6-c2e104b0156f",
@@ -35,9 +38,10 @@ var units = [
         "created_at": 1472731865.298275,
         "room_name": "Tyler Springs",
         "name": "Advanced even-keeled flexibility",
-        "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "FRIDAY", "time" : "11:00"}
+        "code" : "AEKF3324",
+        "icon": "resistance",
+        "status": "closed",
+        "next_session": {"day": "FRIDAY", "time": "11:00"}
     },
     {
         "public_id": "30d28006-9656-4f5b-a20b-1d8f002b9c2a",
@@ -46,8 +50,8 @@ var units = [
         "room_name": "Jimmy Land",
         "name": "Devolved fault-tolerant flexibility",
         "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "TUESDAY", "time" : "12:00"}
+        "status": "closed",
+        "next_session": {"day": "TUESDAY", "time": "12:00"}
     },
     {
         "public_id": "e09b91f7-daf4-4494-9e50-eca3256bb389",
@@ -56,8 +60,8 @@ var units = [
         "room_name": "Medina Vista",
         "name": "Ergonomic tertiary leverage",
         "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "THURSDAY", "time" : "16:00"}
+        "status": "closed",
+        "next_session": {"day": "THURSDAY", "time": "16:00"}
     },
     {
         "public_id": "61186a2b-52b7-4b5d-8c34-a80c4530e30e",
@@ -66,8 +70,8 @@ var units = [
         "room_name": "White Walks",
         "name": "Integrated 24/7 functionalities",
         "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "WEDNESDAY", "time" : "17:00"}
+        "status": "closed",
+        "next_session": {"day": "WEDNESDAY", "time": "17:00"}
     },
     {
         "public_id": "f3eeed02-faf6-4155-905a-d803c97e9b61",
@@ -76,8 +80,8 @@ var units = [
         "room_name": "Brooke Field",
         "name": "Future-proofed content-based framework",
         "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "FRIDAY", "time" : "12:00"}
+        "status": "closed",
+        "next_session": {"day": "FRIDAY", "time": "12:00"}
     },
     {
         "public_id": "f385db35-3b11-4136-b4b0-86aef9bcbe79",
@@ -86,8 +90,8 @@ var units = [
         "room_name": "Gibson Ridges",
         "name": "Innovative web-enabled instruction set",
         "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "TUESDAY", "time" : "9:00"}
+        "status": "closed",
+        "next_session": {"day": "TUESDAY", "time": "9:00"}
     },
     {
         "public_id": "651fa1eb-7407-4765-ba86-73b5a840d0f0",
@@ -96,8 +100,8 @@ var units = [
         "room_name": "Timothy Isle",
         "name": "Integrated foreground infrastructure",
         "icon": null,
-        "status" : "closed",
-        "next_session" : {"day" : "MONDAY", "time" : "12:00"}
+        "status": "closed",
+        "next_session": {"day": "MONDAY", "time": "12:00"}
     }]
 
 
