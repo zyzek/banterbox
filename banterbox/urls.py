451542450b01470ab906e4cac160c45c1b8792a4
Wafik Salim <wafiks@gmail.com>
2016-09-16 01:21:04 +1000

    Moved auth route into app rather than project

--- banterbox/urls.py
+++ banterbox/urls.py
@@ -2,18 +2,20 @@ from django.conf.urls import url, include
 from django.conf.urls import url
 from rest_framework import routers
 from . import views
+from rest_framework.authtoken import views as auth_views
+
 
 router = routers.DefaultRouter()
-router.register(r'api/profile', views.ProfileViewSet)
-router.register(r'api/room-status', views.RoomStatusViewSet)
-router.register(r'api/room', views.RoomViewSet)
-router.register(r'api/comment', views.CommentViewSet)
-router.register(r'api/user', views.UserViewSet)
-router.register(r'api/user-role', views.UserRoleViewSet)
-router.register(r'api/user-unit-role', views.UserUnitRoleViewSet)
-router.register(r'api/unit', views.UnitViewSet)
-router.register(r'api/user-unit-enrolment', views.UserUnitEnrolmentViewSet)
-router.register(r'api/scheduled-room', views.ScheduledRoomViewSet)
+router.register(r'profile', views.ProfileViewSet)
+router.register(r'room-status', views.RoomStatusViewSet)
+router.register(r'room', views.RoomViewSet)
+router.register(r'comment', views.CommentViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'user-role', views.UserRoleViewSet)
+router.register(r'user-unit-role', views.UserUnitRoleViewSet)
+router.register(r'unit', views.UnitViewSet)
+router.register(r'user-unit-enrolment', views.UserUnitEnrolmentViewSet)
+router.register(r'scheduled-room', views.ScheduledRoomViewSet)
 
 
 
@@ -23,7 +25,7 @@ router.register(r'api/scheduled-room', views.ScheduledRoomViewSet)
 # Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/auth/', auth_views.obtain_auth_token),
+    url(r'^api/', include(router.urls)),
     url(r'^$', views.index, name='index'),
 ]
