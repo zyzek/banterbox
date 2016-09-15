222b6fb527a014041f04cb40b786a0256e47d973
Alexander Collins <alexcollins010@gmail.com>
2016-09-15 18:02:39 +1000

    adding Django Rest Test

--- banterbox/tests.py
+++ banterbox/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from myproject.apps.core.models import Account
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+#account tests
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('account-list')
+        print(url)
+
+        data = {'name': 'myusername'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Account.objects.count(), 1)
+        self.assertEqual(Account.objects.get().name, 'myusername')
+
+#
