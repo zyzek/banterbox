f51d40c1aabd120d762d2712422a80329ac938af
Wafik Salim <wafiks@gmail.com>
2016-09-16 12:06:47 +1000

    Created stub for user role decorator

--- banterbox/serializers.py
+++ banterbox/serializers.py
@@ -1,8 +1,5 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from banterbox.models import *
-
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
