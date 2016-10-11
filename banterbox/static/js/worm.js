6899bcb562f4dd854cffd26b47a2879beadfc1a0
Dominic Romanowski <Dom@DomRom.local>
2016-10-11 15:02:46 +1100

    comments display on worm and scaling height

--- banterbox/static/js/worm.js
+++ banterbox/static/js/worm.js
@@ -8,11 +8,15 @@ canvas.width = width;
 canvas.height = height;
 
 var worm = [];
+var buffer = [];
+var time = 0;
 var mouse_x = 0;
+var max_worm_val = 1;
+var users = 50;
+var worm_length = 200;
+var test_scaling_height = 1;
 
-var max_values = 500;
-
-var data = [
+var comment_data = [
     {
         time: 27,
         user: "acol6969",
@@ -33,11 +37,11 @@ var data = [
         user: "acol6969",
         comment: "terrible lecture!"
     }
-];
+]
 
 var key_codes = {
     ENTER: 13,
-    SPACE: 32,
+    SPACE: 32
 }
 
 
@@ -60,6 +64,13 @@ window.addEventListener('resize', function(){
     height = canvas.height = window.innerHeight
 })
 
+function parse_comment_data(data) {
+  console.log(data) //TODO
+}
+function parse_vote_data(data) {
+  console.log(data) //TODO
+}
+
 /**
  * Restarts the canvas data if enter is pressed
  * @param e
@@ -74,13 +85,12 @@ function handleMouseMove(e) {
     mouse_x = e.clientX;
 }
 
-
-
 function setNormalFill() {
     context.fillStyle = "rgb(239, 5, 239)";
     context.strokeStyle = "rgb(239, 5, 239)";
     context.lineWidth = 10;
 }
+
 function setThinLine() {
     context.fillStyle = "rgb(255, 255, 255)";
     context.strokeStyle = "rgb(255, 255, 255)";
@@ -93,6 +103,20 @@ function setCommentFill() {
 }
 
 
+function roundRect(x, y, w, h, r) {
+  // from: http://stackoverflow.com/a/7838871
+  if (w < 2 * r) r = w / 2;
+  if (h < 2 * r) r = h / 2;
+  context.beginPath();
+  context.moveTo(x+r, y);
+  context.arcTo(x+w, y,   x+w, y+h, r);
+  context.arcTo(x+w, y+h, x,   y+h, r);
+  context.arcTo(x,   y+h, x,   y,   r);
+  context.arcTo(x,   y,   x+w, y,   r);
+  context.fill();
+  context.closePath();
+  return this;
+}
 
 
 
@@ -107,35 +131,64 @@ function draw_at_point(x, y) {
     context.closePath();
 }
 
-function draw_comments(left_bound, right_bound) {
+function draw_comments(left_bound, right_bound, mouse_x_point) {
     setCommentFill();
     context.beginPath();
-    for (let i = 0; i < data.length; i++) {
-        let comment = data[i];
+    let min_comm_dist = 100; // the max distance from the mouse that a comment will pop up
+    // min_comm_dist(px) on either side of comment, or the closest one
+    // TODO: HCI STUDY ON WHAT VALUES BASED ON WHICH SCREEN WIDTHS
 
+    let min_comm = null;
+
+    for (let i = 0; i < comment_data.length; i++) {
+        let comment = comment_data[i];
         if (comment.time > left_bound || comment.time < right_bound) {
+
+
             let x = (comment.time - left_bound) * ((width - 30) / (right_bound - left_bound - 1));
             let y = height - 30;
             context.arc(x, y, 10, 0, 2 * Math.PI);
             context.fill();
+
+            if (Math.abs(mouse_x_point-x) < min_comm_dist) {
+              min_comm_dist = Math.abs(mouse_x_point-x);
+              min_comm = comment;
+            }
         }
     }
-    context.closePath();
+    context.closePath()
+
+    let padding = 5 // how many "time units" on either side of the comment to pick up
+                    // 0 for only exact same time as the one selected
+
+    if (min_comm) {
+      let to_display = []
+      for (let i = 0; i < comment_data.length; i++) {
+        if (comment_data[i].time-padding <= min_comm.time && comment_data[i].time+padding >= min_comm.time ) {
+          to_display.push(comment_data[i])
+        }
+      }
+      context.strokeStyle = "black"
+
+      for (let i=0; i<to_display.length; i++) {
+        context.fillStyle = "white"
+        let x = (min_comm.time - left_bound) * ((width - 30) / (right_bound - left_bound - 1));
+        let y = 30+i*42;
+        //context.moveTo(x,y);
+        roundRect(x, y, 150, 40, 4)
+        context.stroke()
+
+        context.font="12px Georgia";
+        context.fillStyle = "black"
+        context.fillText(to_display[i].user,x+5, y+15);
+        context.fillText(to_display[i].comment,x+5, y+30);
+
+      }
+
+    }
+
 }
 
-//var rainbow_colours = ['ff0000', 'ffff00', '00ff00', '0000ff'];
-//
-//function set_rainbow_colour(x,min_x,max_x) {
-//
-//    if (x < (max_x-min_x)/3) {
-//        context.strokeStyle = "rgb(255," +(255 * (x / ((max_x - min_x) / 3)))+ ",0)";
-//    } else if (x < 2*(max_x-min_x)/3) {
-//        context.strokeStyle = "rgb("+(255-255*((x-(max_x-min_x)/3)/((max_x-min_x)/3))) + ",255,0)";
-//    }
-//    else if (x < 3*(max_x-min_x)/3) {
-//        context.strokeStyle =  "rgb(0,"+ (255 - 255 * ((x - 2 * (max_x - min_x) / 3) / ((max_x - min_x) / 3))) +"," +( 255 * ((x - 2 * (max_x - min_x) / 3) / ((max_x - min_x) / 3)))+")";
-//    }
-//}
 
 
 function draw_worm(worm, zoom = 100) {
@@ -164,12 +217,13 @@ function draw_worm(worm, zoom = 100) {
 
         for (let i = 1; i < worm.length; i++) {
             let x = i * ((width - 30) / (worm.length - 1));
+            let y = worm[i]*height/(max_worm_val*2)+(height/2)
             if (!mouseFound && x > mouse_x) {
                 mouseFound = true;
                 _x = x;
-                _y = worm[i];
+                _y = y;
             }
-            context.lineTo(x, worm[i]);
+            context.lineTo(x, y);
 
         }
 
@@ -177,12 +231,13 @@ function draw_worm(worm, zoom = 100) {
         context.moveTo(0, worm[worm.length - zoom - 1]);
         for (let i = 1; i < zoom; i++) {
             let x = (i) * ((width - 30) / (zoom - 1));
+            let y = worm[worm.length - zoom + i - 1]*height/(max_worm_val*2)+(height/2)
             if (!mouseFound && x > mouse_x) {
                 mouseFound = true;
                 _x = x;
-                _y = worm[worm.length - zoom + i - 1];
+                _y = y;
             }
-            context.lineTo(x, worm[worm.length - zoom + i - 1]);
+            context.lineTo(x, y);
         }
     }
     context.stroke();
@@ -190,9 +245,9 @@ function draw_worm(worm, zoom = 100) {
 
     draw_at_point(_x, _y);
     if (worm.length > zoom) {
-        draw_comments(worm.length - zoom, worm.length);
+        draw_comments(worm.length - zoom, worm.length, _x);
     } else {
-        draw_comments(0, worm.length);
+        draw_comments(0, worm.length, _x);
     }
 }
 
@@ -202,21 +257,14 @@ function draw_worm(worm, zoom = 100) {
  * TODO : Add comments to explain what's being updated here
  */
 function update() {
+  //zoom = document.getElementById("myRange").value;
 
-    // Prettier fake data!
-    let change_rate = 9.45
-    let y = (Math.random() * change_rate) * (Math.random() > 0.5 ? 1 : -1)
-    y += worm[worm.length -1]
+  //test_scaling_height *= 1.01
+    let vote_total = (Math.random()*users*test_scaling_height) * (Math.random() > 0.5 ? 1 : -1)
+    if (Math.abs(vote_total) > max_worm_val)
+      max_worm_val = Math.abs(vote_total)
 
-    // Make sure it stays within screen limitations
-    if(y < 0){
-        y = 0;
-    }else if(y > window.innerHeight){
-        y  = window.innerHeight
-    }
-
-
-    worm.push(y);
+    worm.push(vote_total);
 }
 
 /**
@@ -224,7 +272,7 @@ function update() {
  */
 function render(){
     context.clearRect(0, 0, width, height);
-    draw_worm(worm, max_values);
+    draw_worm(worm, worm_length);
 }
 
 
