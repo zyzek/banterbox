d49119a601a7fa3e4a7245e54ec9100b1cbe67b1
root <root@ip-172-31-34-148.us-west-2.compute.internal>
2016-10-11 03:49:27 +0000

    Changed database to use postgres & set it to use environment vars

--- project/settings.py
+++ project/settings.py
@@ -87,9 +87,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME'  : os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -128,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
