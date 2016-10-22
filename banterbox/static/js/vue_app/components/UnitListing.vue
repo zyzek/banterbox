c7b2c71a6779d576f9e3a445a8fa261a03fa9206
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2016-10-22 13:30:46 +1100

    Seeder + css fixes. Universal dummy unit generation.

--- banterbox/static/js/vue_app/components/UnitListing.vue
+++ banterbox/static/js/vue_app/components/UnitListing.vue
@@ -12,13 +12,13 @@
     <div v-if="store.units.units.length > 0">
         <div>
             <div class="row">
-                <unit-panel :class="{blur: [null,unit.public_id].indexOf(store.rooms.hovered) < 0}"
+                <unit-panel :class="{blur: [null,unit.public_id].indexOf(store.units.hovered) < 0}"
                             v-for="unit in store.units.units"
                             :unit="unit"></unit-panel>
             </div>
         </div>
 
-        <div class="row" :class="{blur: !!store.rooms.hovered}">
+        <div class="row" :class="{blur: !!store.units.hovered}">
             <div class="col-xs-12" style="font-size: 0.85rem;">
                 <div>Here's your classes. This line of text is to make the page look like it knows what
                     it's doing. Ahoy mihoy.
